eb446c5cd48d0d5d4a96871befbb3cd6082ef11a
tup37064 <shawn.dutill@temple.edu>
2023-09-15 16:06:48 -0400

    HTML & typescript is rendered properly in browser

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+// TypeScript code that manipulates the DOM and adds content to the 'root' div
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    if (root) {
+        console.log("Hello World!");
+        root.innerHTML = '<h2>Hello, World!</h2>';
+    }
+});
+//# sourceMappingURL=index.js.map
